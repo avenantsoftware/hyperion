d328ef6b49ee7f328c4907b079b6a8b992f0dfba
Avenant Software <avenantsoftware@users.noreply.github.com>
2018-05-07 18:50:28 +0200

    Update Logger.cpp
    fix macOS compile errors

--- libsrc/utils/Logger.cpp
+++ libsrc/utils/Logger.cpp
@@ -62,11 +62,8 @@ Logger::Logger ( std::string name, LogLevel minLevel ):
 	
 	if (_syslogEnabled && loggerCount == 1 )
 	{
-		#ifndef ENABLE_OSX
-		openlog (program_invocation_short_name, LOG_CONS | LOG_PID | LOG_NDELAY, LOG_LOCAL0);
-		#else
-		openlog (getprogname(), LOG_CONS | LOG_PID | LOG_NDELAY, LOG_LOCAL0);
-		#endif
+
+		openlog (_appname, LOG_CONS | LOG_PID | LOG_NDELAY, LOG_LOCAL0);
 	}
 }
 
